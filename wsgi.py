56812137b14314d395358a3c2b20ee0281ec9787
Rob-MFn-Fletcher <robroy.fletcher@gmail.com>
2017-03-29 22:44:47 -0700

    app -> application

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
 import flask
 
-app = flask.Flask(__name__)
+application = flask.Flask(__name__)
 
 @app.route('/')
 def homepage():
     return flask.render_template('index.html')
 
 if __name__=='__main__':
-    app.run()
+    applicatio.run()
