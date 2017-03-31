8138acf0917b5963af8ea715336f64b1d5684c0b
Rob-MFn-Fletcher <robroy.fletcher@gmail.com>
2017-03-31 13:41:22 -0700

    Adding setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cernhomepage',
+    packages=['cernhomepage'],
+    include_package_data=True,
+    install_requires=[
+        'flask', 'gunicorn'
+    ],
+)
