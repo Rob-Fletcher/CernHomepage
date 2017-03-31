a1ac2e670474d62e09ea3675343a84a06b0880dc
Rob-MFn-Fletcher <robroy.fletcher@gmail.com>
2017-03-31 09:39:44 -0700

    Trying to just do a print

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,8 @@ application = flask.Flask(__name__)
 
 @application.route('/')
 def homepage():
-    return flask.render_template('index.html')
+    #return flask.render_template('index.html')
+    return "hello world"
 
 if __name__=='__main__':
     application.run()
